8134da09b3b08f3d10929bf085ac8127f61cde72
Sukie Oduwole <sukieoduwole@aol.com>
2023-09-16 18:16:40 +0100

    made a correction with the codes

--- shellScripting/myscript.sh
+++ shellScripting/myscript.sh
@@ -142,7 +142,7 @@
 
 
 # CREATE FOLDER AND WRITE TO A FILE
-mkdir hello
-touch "hello/world.txt"
-echo "Hello World" >> "hello/world.txt"
-echo "Created hello/world.txt"
+#mkdir hello
+#touch "hello/world.txt"
+#echo "Hello World" >> "hello/world.txt"
+#echo "Created hello/world.txt"
